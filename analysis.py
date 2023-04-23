aa1f12fb89fda697f9daa73a2c8d5004545ea335
unknown <waszka04@gmail.com>
2023-04-23 11:28:33 +0100

    import data

--- analysis.py
+++ analysis.py
@@ -1,2 +1,17 @@
 # Project 
 #Agnieszka Waszczuk 
+
+# import library pandas
+import pandas as pd
+
+# create a list with new column names
+headers=['sepal_lenght_cm','sepal_width_cm','petal_lenght_cm','petal_width_cm','class']
+
+# read csv file without the headers
+iris = pd.read_csv('iris.data',header=None)
+
+# assign new header names to dataset
+iris.columns = headers
+
+# print the table
+print(iris)
