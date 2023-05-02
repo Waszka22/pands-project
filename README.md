c69f946188e91588fcfb4daeee4ed807ec7c061c
unknown <waszka04@gmail.com>
2023-05-02 23:35:23 +0100

    addingg correlation

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ The Data was extracted from Fisher's Irish data set. The Famous database from Fi
 ___
 
 ### Description about database
-
-
+The Iris Data set has 150 rows and 5 colums. 
+No missing data in this dataset
+The first column represented sepal length, the second column represented sepal width, the third column represented petal length, and the fourth column represented petal width
 
 ### Source 
 
@@ -16,3 +17,8 @@ ___
 * math 
 * pandas 
 * matplotlib. pyplot 
+
+### # Reference: 
+# https://towardsdatascience.com/dimension-reduction-techniques-with-python-f36ca7009e5c
+# https://www.kaggle.com/code/jchen2186/machine-learning-with-iris-dataset
+# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
--- analysis.py
+++ analysis.py
@@ -21,27 +21,60 @@ iris.head(10)
 print(iris.describe())
 
 
-# import the numpy and matplotlib.pyplot
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA, KernelPCA
-from sklearn.datasets import make_circles
+#The first one is the number of rows and the number of columns.
+iris.shape
 
-# use random.normal() method to create a list with data
-# to draw plot random samples from a normal distribution
-np.random.seed(0)
-X, y = make_circles(n_samples=400, factor=.3, noise=.05)
+# The column() function prints all the columns of the dataset in a list form.
+iris.columns
 
-# add a title for plot
-plt.figure(figsize=(10,10))
-plt.subplot(2, 2, 1, aspect='equal')
-plt.title("Original space")
+# Checkig missing datait will display True else False.
+iris.isnull()
 
-reds = y == 0
-blues = y == 1
+#correlation is used to determine pairwise correlation of all the columns of the dataset
+iris.corr(method='pearson')
 
-# add labels to axises
-plt.scatter(X[reds, 0], X[reds, 1], c="red",s=20, edgecolor='k')
-plt.scatter(X[blues, 0], X[blues, 1], c="blue",s=20, edgecolor='k')
-plt.xlabel("$x_1$")
-plt.ylabel("$x_2$")
+#print (iris.info())
+
+
+# # Data Visualization 
+# # draw pair plot 
+# tmp = iris.drop("Id", axies=1)
+# g = sns.pairplot (tmp, hue= 'Species')
+
+
+
+
+
+# # 
+
+
+
+# # import the numpy and matplotlib.pyplot
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.decomposition import PCA, KernelPCA
+# from sklearn.datasets import make_circles
+
+# # use random.normal() method to create a list with data
+# # to draw plot random samples from a normal distribution
+# np.random.seed(0)
+# X, y = make_circles(n_samples=400, factor=.3, noise=.05)
+
+# # add a title for plot
+# plt.figure(figsize=(10,10))
+# plt.subplot(2, 2, 1, aspect='equal')
+# plt.title("Original space")
+
+# reds = y == 0
+# blues = y == 1
+
+# # add labels to axises
+# plt.scatter(X[reds, 0], X[reds, 1], c="red",s=20, edgecolor='k')
+# plt.scatter(X[blues, 0], X[blues, 1], c="blue",s=20, edgecolor='k')
+# plt.xlabel("$x_1$")
+# plt.ylabel("$x_2$")
+
+
+# # # Reference: 
+# # # https://towardsdatascience.com/dimension-reduction-techniques-with-python-f36ca7009e5c
+# # # https://www.kaggle.com/code/jchen2186/machine-learning-with-iris-dataset
